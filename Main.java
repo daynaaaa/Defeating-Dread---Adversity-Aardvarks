5d2cfff219c892da580a1342cdc38715112009ab
daynaaaa <100596357+daynaaaa@users.noreply.github.com>
2022-05-17 10:14:14 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,38 +6,67 @@
  *
  * Variables:
  *
- * Version #1 May 16 - May 20 - [30 mins]:
- *    [author] - [modification] 
+ * Version #1 May 16 - May 20 - [1.5 hrs]:
+ *    Danya - [modification] 
  *    
  */
- import java.util.*;
+import java.util.*;
 import java.io.*;
 import javax.swing.*;
 import java.awt.*;
 public class Main extends JPanel{
-	public static char c;
+	public char c;
+   /**the current screen to be displayed*/
+   public static JPanel current;
+   /**the current frame*/
+   public static JFrame frame;
+   //public static Graphics2D graphics;
    public void paintComponent(Graphics g)
+   {
+      super.paintComponent(g);  //canvas
+      int w = getWidth();
+      int h = getHeight();
+      g.setColor(Color.RED);
+      g.drawRect(w/4, h/4, w/2, h/2);  //rectangle is proportionate to canvas
+      g.setColor(Color.BLUE);
+      g.setFont(new Font("Serif", Font.PLAIN, 18));
+      g.drawString("SUCCESS!", w/4 + 15, h/2 + 5);
+   }
+   /**
+     * Initialize the JFrame
+   */
+   private static void frame() {
+      frame = new JFrame();
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setTitle("Defeating Dread");
+      frame.pack();
+      frame.setVisible(true);
+      frame.setSize(1000, 900);
+      frame.setResizable(false);
+      frame.setLocationRelativeTo(null);
+   }
+   public static void main(String[] args)
   {
-       super.paintComponent(g);  //canvas
-       int w = getWidth();
-       int h = getHeight();
-       g.setColor(Color.RED);
-       g.drawRect(w/4, h/4, w/2, h/2);  //rectangle is proportionate to canvas
-       g.setColor(Color.BLUE);
-       g.setFont(new Font("Serif", Font.PLAIN, 18));
-       g.drawString("SUCCESS!", w/4 + 15, h/2 + 5);
-  }
-	public static void main(String[]args) {
-		Main m = new Main();
-      /*JFrame window = new JFrame("Drawings");
-       window.setBounds(100, 100, 300, 200);
-       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-       JPanel canvas = new Main();
-       canvas.setBackground(Color.WHITE);
-       window.getContentPane().add(canvas);
-       window.setVisible(true);
-*/
-		//runMenu();
-      //c = getC();
+
+      Main m = new Main();
+      SwingUtilities.invokeLater(Main::frame);
+
+      JPanel panel = new JPanel();
+      panel.setBackground(Color.WHITE);
+      //panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      panel.setLayout(new GridLayout(0, 1));
+      
+      /*
+      SplashScreen splash = new SplashScreen();
+      frame.add (splash);
+      splash.play();
+      current = splash;*/
+      while (frame.isVisible()) {
+            frame.remove(current);
+            Menu m2 = new Menu();
+            frame.add(m2);
+            
+            current = m2;
+      }
 	}
 }
--- Menu.java
+++ Menu.java
@@ -6,21 +6,37 @@
  *
  * Variables:
  *
- * Version #1 May 16 - May 20 - [1 hr]:
- *    [author] - [modification] 
+ * Version #1 May 16 - May 20 - [2 hr]:
+ *    Danya - [modification] 
  *    
  */
 import java.util.*;
 import java.io.*;
 import javax.swing.*;
-public class Menu {
-	public static char c;
+import java.awt.*;
+public class Menu extends JPanel{
+	public char c;
 	public Menu() {
 		c = '0';
 	}
-	public static char getC() {
+	public char getC() {
 		return c;
 	}
+   /**
+     * 
+     *
+     * @param g the graphics to be drawn
+     */
+   @Override
+   public void paintComponent(Graphics g)
+   {
+      super.paintComponent(g);
+      int x0 = 40;
+      int y0 = 40;
+      g.setColor(Color.BLACK);
+      g.drawRect(x0 - 15, y0, 30, 20);
+      g.drawOval(x0 - 10, y0 - 20, 20, 20);
+   }
 	public static void runMenu() {
 		Menu m = new Menu();
       Scanner in = new Scanner(System.in);
@@ -29,8 +45,8 @@ public class Menu {
 		System.out.println("Play Game\nInstructions\nQuit");
 		while(true) {
 			try {
-				c = in.next().charAt(0);
-				if(c < 49 || c > 51){
+				m.c = in.next().charAt(0);
+				if(m.c < 49 || m.c > 51){
 					throw new IllegalArgumentException();
 				}
             else break;
