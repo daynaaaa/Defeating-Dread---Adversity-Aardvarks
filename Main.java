337a22b721e4426f38eee3b8f5e72839c4708dc2
daynaaaa <100596357+daynaaaa@users.noreply.github.com>
2022-05-17 09:19:54 -0400

    Add files via upload

--- Main.java
+++ Main.java
@@ -6,10 +6,38 @@
  *
  * Variables:
  *
- * Version #1 May 16 - May 20 - [time spent]:
+ * Version #1 May 16 - May 20 - [30 mins]:
  *    [author] - [modification] 
  *    
  */
-public class Main {
-
+ import java.util.*;
+import java.io.*;
+import javax.swing.*;
+import java.awt.*;
+public class Main extends JPanel{
+	public static char c;
+   public void paintComponent(Graphics g)
+  {
+       super.paintComponent(g);  //canvas
+       int w = getWidth();
+       int h = getHeight();
+       g.setColor(Color.RED);
+       g.drawRect(w/4, h/4, w/2, h/2);  //rectangle is proportionate to canvas
+       g.setColor(Color.BLUE);
+       g.setFont(new Font("Serif", Font.PLAIN, 18));
+       g.drawString("SUCCESS!", w/4 + 15, h/2 + 5);
+  }
+	public static void main(String[]args) {
+		Main m = new Main();
+      /*JFrame window = new JFrame("Drawings");
+       window.setBounds(100, 100, 300, 200);
+       window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+       JPanel canvas = new Main();
+       canvas.setBackground(Color.WHITE);
+       window.getContentPane().add(canvas);
+       window.setVisible(true);
+*/
+		runMenu();
+      c = getC();
+	}
 }
