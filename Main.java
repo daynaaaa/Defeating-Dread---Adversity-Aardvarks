16ea0232e1f5f482c0ce124f744e2cbcbcd427f9
myae2 <mya.epstein@student.tdsb.on.ca>
2022-05-17 09:56:12 -0400

    changes made for Level() methods

--- Main.java
+++ Main.java
@@ -37,7 +37,7 @@ public class Main extends JPanel{
        window.getContentPane().add(canvas);
        window.setVisible(true);
 */
-		runMenu();
-      c = getC();
+		//runMenu();
+      //c = getC();
 	}
 }
