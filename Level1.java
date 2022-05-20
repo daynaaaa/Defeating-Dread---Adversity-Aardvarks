a65d60f232f3f20ccaae6b463065b8e071e37a8e
sulagnanandi <101913994+sulagnanandi@users.noreply.github.com>
2022-05-20 09:34:15 -0400

    Update and rename LevelOne.java to Level1.java

--- Level1.java
+++ Level1.java
@@ -1,10 +1,24 @@
+/**
+ * Authors: Danya C, Mya E, Sulagna N
+ * Teacher: Ms. Krasteva
+ * Date: May 16, 2022
+ * Desc: "Level1" class that will run the second level when called
+ *
+ * Version #2 May 16 - May 20 - [0.5 hrs]:
+ *    Sulagna - created preliminary Level1 class with some methods
+ *    Does not draw anything
+ *    Calls Talk(ArrayList<String>)
+ *
+ */
+
+
 import java.util.*;
 
-public class LevelOne {
+public class Level1 {
 
 	private ArrayList<String> lesson;
 
-	public LevelOne() {
+	public Level1() {
 		lesson = new ArrayList<String>();
 		fillArrayList();
 	}
