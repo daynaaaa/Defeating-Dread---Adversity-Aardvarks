27c2b6d8e11d9965b7a4a230817019ccffaf92bd
daynaaaa <100596357+daynaaaa@users.noreply.github.com>
2022-06-03 21:14:49 -0400

    Add files via upload

--- Level3.java
+++ Level3.java
@@ -6,8 +6,10 @@
  *
  * Version #1 May 24 - May 27 - [1 hrs]:
  *    Danya - added messages, which the user can go through by pressing space
- * Version #2 May 30 - June 3 - [2 hrs]:
+ * Version #2 May 30 - June 3 - [5 hrs]:
  *    Danya - added squares to represent the characters and the tools
+ *    Added the function to view the tools details
+ *    Also implemented fighting the monster
  *
  */
 
@@ -39,22 +41,42 @@ public class Level3 extends JPanel{
     private boolean isMonster;
     /** If the user is reading dialogue */
     private boolean isDialogue;
+    /** If the user has chosen a tool yet */
+    private boolean toolChosen;
+    /** If the user has completed the task*/
+    private boolean taskComplete;
+    /**If the user is currently doing a task*/
+    private boolean isTask;
+    /** The current monster the user is fighting */
+    private Monster m;
+    /** the number of the monster the user is fighting*/
+    private int monNum;
+    /* The health of the monster*/
+    private int health;
     /**
      * constructor
      *
      * @param cT chosen tools stored
      */
     public Level3(ArrayList<Tool> cT) {
-    //public Level3() {
          //cT.get(0).getName();
+         health = 10;
+         monNum = 1;
+         m = new Monster(health);
          chosenTools = cT;
          end = false;
          isDialogue = true;
+         taskComplete = true;
          getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW).put(KeyStroke.getKeyStroke("SPACE"), "next");
          getActionMap().put("next", new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(isDialogue) count++;
+                if(isDialogue)count++;
+                //System.out.println(isDialogue);
+                else if(isTask)taskComplete = true;
+                /*else {
+                  taskComplete = true;
+                }*/
                 repaint();
             }
          });
@@ -65,21 +87,49 @@ public class Level3 extends JPanel{
                 //choosing tools
                 if(count >= 4){
                    if (x > 170 && y > 370 && x < 250 && y < 450) {
+                       if(currentTool == null){
+                           toolChosen = true;   
+                       }
+                       else if(currentTool.getName() != chosenTools.get(0).getName()){
+                           toolChosen = true;   
+                       }
+                       else toolChosen = false;
                        currentTool = chosenTools.get(0);
                        tool = 1;
+                       //toolChosen = true;
+                       taskComplete = false;
                        repaint();
                    }
                    else if (x > 270 && y > 370 && x < 350 && y < 450) {
-                       currentTool = chosenTools.get(1);
+                       if(currentTool == null){
+                           toolChosen = true;   
+                       }
+                       else if(currentTool.getName() != chosenTools.get(1).getName()){
+                           toolChosen = true;   
+                       }
+
+                       else toolChosen = false;
                        tool = 2;
+                       //toolChosen = true;
+                       taskComplete = false;
                        repaint();
                    }
                    else if (x > 370 && y > 370 && x < 450 && y < 450) {
+                       if(currentTool == null){
+                           toolChosen = true;   
+                       }
+                       else if(currentTool.getName() != chosenTools.get(2).getName()){
+                           toolChosen = true;   
+                       }
+                       else toolChosen = false;
                        currentTool = chosenTools.get(2);
                        tool = 3;
+                       //toolChosen = true;
+                       taskComplete = false;
                        repaint();
                    }
                 }
+                repaint();
             }
         });
     }
@@ -106,7 +156,13 @@ public class Level3 extends JPanel{
            g.fillRect(270, 370, 80, 80);
            g.fillRect(370, 370, 80, 80);
            g.setColor(Color.WHITE);
-           g.drawString(currentTool.getName(), 20, 20);
+           g.fillRect(20, 10, 400, 150);
+           g.setColor(Color.BLACK);
+           g.drawString(currentTool.getName()+" " + currentTool.getInfo(), 20, 20);
+           for(int i = 0;i < currentTool.getTask().size();i++){
+               g.drawString(currentTool.getTask().get(i), 20, i*20+40);
+           }
+           
         }
         else if(tool == 2){
            g.setColor(Color.GREEN);
@@ -137,25 +193,53 @@ public class Level3 extends JPanel{
                break;
             case 4:
                isMonster = true;
+               
+               //monster:
+               g.setColor(Color.WHITE);   
+               g.fillRect(500, 100, 260, 350);
+               g.setColor(Color.RED);   
+               g.fillRect(500, 50, 260, 20);
+               g.setColor(Color.GREEN);
+               g.fillRect(500, 50, m.getHealth()*26, 20);
+               g.setColor(Color.WHITE);  
+               g.drawString("Monster number: " + monNum, 500, 10);
+               g.drawString("Health: " + health, 500, 30);
                isDialogue = false;
-               for(int i = 1; i <= 5; i++){
-                  Monster m = new Monster(i*10);
-                  /*if(!m.fight(chosenTool)){
-                     //kill
-                  }*/
+               
+               if(toolChosen&& monNum <= 5){
+                  isTask = true;
+                  if(taskComplete){
+                  //System.out.println(m.getHealth());
+                  if(m.fight(currentTool)&&monNum != 5){
+                        monNum++;
+                        health = monNum * 10;
+                        m = new Monster(health);
+                  }
+                  else health = m.getHealth();
+                  toolChosen = false;
+                  System.out.println(m.getHealth());
+                  }
                }
+               else if (monNum == 5) count++;
+                
+               //isDialogue = true;
                break;
             
             case 5:
+               isTask = false;
                end = true;
                break;
             default:
             
         }
-        if(isMonster){
+        /*if(isMonster){
             g.setColor(Color.WHITE);   
             g.fillRect(500, 100, 260, 350);
-        }
+            g.setColor(Color.RED);   
+            g.fillRect(500, 50, 260, 20);
+            g.setColor(Color.GREEN);
+            g.fillRect(500, 50, m.getHealth()*26, 20);
+        }*/
         
     }
     /**
